2c93189f4551f1bac570f7f70fba259c6e85ace4
Muhammad Maulana Yusuf <mhm.maulanayusuf@gmail.com>
2023-01-06 00:55:57 +0700

    Contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MMaulanaYusuf\BelajarComposer;
+
+class Customer {
+
+    function __construct(private string $name) {}
+
+    function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+
+}
